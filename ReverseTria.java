be56c87bad1f0dde3c6e2762844290b1c1d4d43c
Shantanu Danej <156588003+Shantanu675@users.noreply.github.com>
2025-01-04 22:40:14 +0530

    Add files via upload

--- ReverseTria.java
+++ ReverseTria.java
@@ -1,12 +1,12 @@
 package Pattern;
 
-public class Square {
+public class ReverseTria {
     public static void main(String[] args) {
         pattern(6);
     }
     static void pattern(int n){
         for(int i = 1; i <= n; i++){
-            for(int j = 1; j <=n ; j++){
+            for(int j = n; j >= i ; j--){
                 System.out.print("x ");
             }
             System.out.println();
